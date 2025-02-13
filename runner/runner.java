7e05b7715fab7d0c68324105e37b64d7d9095e3f
Jason Alvarenga <JasonAlvarenga64@gmail.com>
2025-02-12 19:17:44 -0500

    fortnite 2

---
+++ runner/runner.java
@@ -0,0 +1,9 @@
+public class runner {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
